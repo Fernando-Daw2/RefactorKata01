07cae84234787b4f2089e7169f35a453ab366e76
Fernando <fpaization@gmail.com>
2021-03-02 16:53:16 +0100

    Add files via upload

--- src/main/java/Alarm.java
+++ src/main/java/Alarm.java
@@ -3,18 +3,18 @@ public class Alarm
     private final double LowPressureThreshold = 17;
     private final double HighPressureThreshold = 21;
 
-    Sensor sensor = new Sensor();
 
     boolean alarmOn = false;
 
-    public void check()
+    public boolean check(Sensor sensor)
     {
         double psiPressureValue = sensor.popNextPressurePsiValue();
 
         if (psiPressureValue < LowPressureThreshold || HighPressureThreshold < psiPressureValue)
         {
-            alarmOn = true;
+            return true;
         }
+        return false;
     }
 
     public boolean isAlarmOn()
